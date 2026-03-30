2db44d986f1f19ed200c4f31d6f51487c6b8817f
Ignacio <ignaciocofre2@gmail.com>
2026-03-30 10:43:26 -0300

    lol

--- code/sorting/scripts/plot_generator.py
+++ code/sorting/scripts/plot_generator.py
@@ -8,13 +8,32 @@ rootDir = (
 )
 
 
-def fixData(data):
-    aux = []
+aux = []
+
+
+def fixData(data, algo):
     for row in data:
         if len(row) == 3:
-            aux.append([int(row[0]), int(row[1]), int(row[2])])
-    aux.sort()
-    return np.array(aux)
+            aux.append([int(row[0]), int(row[1]), int(row[2]), algo])
+
+
+def buildPlots(path, xVar, yVar, df):
+    plt.figure()
+    ax = sns.lineplot(
+        data=df,
+        x=xVar,
+        y=yVar,
+        hue="algorithm",
+        estimator="mean",
+        style="algorithm",
+        markers=True,
+        dashes=False,
+        palette="flare",
+    )
+    ax.set_xscale("log")
+    ax.set_yscale("log")
+    plt.savefig(path)
+    plt.close()
 
 
 sortDataPath = rootDir.format("measurements", "s.txt")
@@ -30,12 +49,18 @@ sortData = [row.split(",") for row in rawSortData.read().split("|")]
 mergeData = [row.split(",") for row in rawMergeData.read().split("|")]
 quickData = [row.split(",") for row in rawQuickData.read().split("|")]
 
-# Creacion de los dataframes para realizar plots
-sortDF = pd.DataFrame(fixData(sortData), columns=["size", "time", "memory"])
-mergeDF = pd.DataFrame(fixData(mergeData), columns=["size", "time", "memory"])
-quickDF = pd.DataFrame(fixData(quickData), columns=["size", "time", "memory"])
+# Eliminacion de filas vacias
+fixData(sortData, "sort")
+fixData(mergeData, "merge")
+fixData(quickData, "quick")
 
-ax = sns.lineplot(data=quickDF, x="size", y="time")
-ax.set_xscale("log")
-ax.set_yscale("log")
-plt.savefig(rootDir.format("plots", "prueba.png"))
+# Creacion del dataframe a partir de datos arreglados
+df = pd.DataFrame(aux, columns=["size", "time", "memory", "algorithm"])
+df.sort_values("size")
+
+# Setteo para usar cuadricula
+sns.set_style("whitegrid")
+
+# Creacion de los graficos
+buildPlots(rootDir.format("plots", "sizeVStime"), "size", "time", df)
+buildPlots(rootDir.format("plots", "sizeVSmemory"), "size", "memory", df)
